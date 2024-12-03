f96ddb4f5e93e9efd8dc94ee31ba53bdd391b497
Wolfgang Richter <wolf@wolfgroup.llc>
2024-12-02 19:44:44 -0500

    aoc2024: getting boilerplate ready

---
+++ 10/10.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    with open('test_input') as fd:
+        for line in fd:
+            print(line)
---
+++ 3/3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from aoc import *
+
+if __name__ == '__main__':
+    with open('test_input') as fd:
+        for line in fd:
+            print(line)
