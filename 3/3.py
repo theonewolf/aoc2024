950a3d9640eaa49dc6dd60df3ce91e9a43000a96
Wolfgang Richter <wolf@wolfgroup.llc>
2024-12-03 19:10:25 -0500

    3: finished part 1

--- 3/3.py
+++ 3/3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from aoc import *
+from re import findall
 
 if __name__ == '__main__':
-    with open('test_input') as fd:
-        for line in fd:
-            print(line)
+    raw_re = r'mul\((\d+),(\d+)\)'
+
+    with open('input') as fd:
+        data = fd.read()
+        matches = findall(raw_re, data)
+        pairs = [(int(x), int(y)) for x, y in matches]
+        print(sum([x*y for x,y in pairs]))
