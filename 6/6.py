4518ec32f540df2a83b5ea1645efa982b3aa345f
Wolfgang Richter <wolf@wolfgroup.llc>
2024-12-06 17:19:28 -0500

    6: part 1 complete

--- 6/6.py
+++ 6/6.py
@@ -1,6 +1,50 @@
 #!/usr/bin/env python3
 
+from pprint import pprint
+
+UP = '^'
+DOWN = 'v'
+LEFT = '<'
+RIGHT = '>'
+OBSTACLE = '#'
+CLEAR = '.'
+# x,y movement
+TURNMOV = {UP : (0, -1), DOWN : (0, 1), LEFT : (-1, 0), RIGHT : (1, 0)}
+TURNMAP = {UP : RIGHT, DOWN : LEFT, LEFT : UP, RIGHT : DOWN}
+DIRECTIONS = {UP, DOWN, LEFT, RIGHT}
+
+class NoNegativeIndexList(list):
+    def __getitem__(self, index):
+        if isinstance(index, int) and index < 0:
+            raise IndexError("Negative indexing is not allowed.")
+        return super().__getitem__(index)
+
+def step(grid):
+    for i, row in enumerate(grid):
+        for j, column in enumerate(row):
+            if column in DIRECTIONS:
+                move_x, move_y = TURNMOV[column]
+                if grid[i + move_y][j + move_x] == OBSTACLE:
+                    grid[i][j] = TURNMAP[column]
+                    return (i, j)
+                else:
+                    grid[i][j] = CLEAR
+                    grid[i + move_y][j + move_x] = column
+                    return (i + move_y, j + move_x)
+
 if __name__ == '__main__':
-    with open('test_input') as fd:
-        for line in fd:
-            print(line)
+    with open('input') as fd:
+        grid = fd.read().split()
+        newgrid = NoNegativeIndexList(grid)
+        for i, row in enumerate(newgrid):
+            newgrid[i] = NoNegativeIndexList(row)
+
+        steps = set()
+        while True:
+            try:
+                steps.add(step(newgrid))
+            except:
+                break
+
+        # This can be off by one, need to account for starting position.
+        print(len(steps))
