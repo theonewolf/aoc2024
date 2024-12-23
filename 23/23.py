172f3ef4489f25eeca5c5875715970247bbe591e
Wolfgang Richter <wolf@wolfgroup.llc>
2024-12-23 07:59:52 -0500

    23: part way, learned about Bron-Kerbosch from ChatGPT today. Not working.  Will rewrite my own solution later.

--- 23/23.py
+++ 23/23.py
@@ -1,6 +1,48 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+from itertools import combinations
+
+def is_clique(graph, nodes):
+    return all(node2 in graph[node1] for node1 in nodes for node2 in nodes if node1 != node2)
+
+def bron_kerbosch(R, P, X, graph, cliques):
+    if not P and not X:
+        cliques.append(sorted(R))  # Canonicalize order
+    for v in sorted(P):  # Sorted to ensure consistent processing order
+        bron_kerbosch(
+            R | {v}, P & graph[v], X & graph[v], graph, cliques
+        )
+        P.remove(v)
+        X.add(v)
+
+def find_all_subcliques(graph, cliques, size):
+    smaller_cliques = []
+    for clique in cliques:
+        if len(clique) >= size:
+            for subset in combinations(sorted(clique), size):
+                if is_clique(graph, subset):
+                    smaller_cliques.append(set(subset))
+    return sorted(map(sorted, smaller_cliques))  # Deduplicate and sort
+
 if __name__ == '__main__':
-    with open('test_input') as fd:
+    with open('input') as fd:
+        computers = set()
+        connections = defaultdict(set)
+
         for line in fd:
-            print(line)
+            a, b = line.strip().split('-')
+            connections[a].add(b)
+            connections[b].add(a)
+
+        print(connections)
+        cliques = []
+        bron_kerbosch(set(), set(connections.keys()), set(), connections, cliques)
+
+        smaller_cliques = find_all_subcliques(connections, cliques, 3)
+
+        cliques += smaller_cliques
+
+        print([c for c in cliques if len(c) == 3])
+        print({tuple(c) for c in cliques if len(c) == 3 and any('t' in n for n in c)})
+        print(len({tuple(c) for c in cliques if len(c) == 3 and any('t' in n for n in c)}))
