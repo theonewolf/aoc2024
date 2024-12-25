528e69974aed86b23b4bb50c3f3f9f7d9b5f85c7
Wolfgang Richter <wolf@wolfgroup.llc>
2024-12-25 08:24:30 -0500

    23: part 1 lol way easier than I thought

--- 23/23.py
+++ 23/23.py
@@ -3,27 +3,17 @@
 from collections import defaultdict
 from itertools import combinations
 
-def is_clique(graph, nodes):
-    return all(node2 in graph[node1] for node1 in nodes for node2 in nodes if node1 != node2)
+def find_cliques(graph):
+    cliques_of_size_3 = set()
 
-def bron_kerbosch(R, P, X, graph, cliques):
-    if not P and not X:
-        cliques.append(sorted(R))  # Canonicalize order
-    for v in sorted(P):  # Sorted to ensure consistent processing order
-        bron_kerbosch(
-            R | {v}, P & graph[v], X & graph[v], graph, cliques
-        )
-        P.remove(v)
-        X.add(v)
+    nodes = sorted(graph.keys())
 
-def find_all_subcliques(graph, cliques, size):
-    smaller_cliques = []
-    for clique in cliques:
-        if len(clique) >= size:
-            for subset in combinations(sorted(clique), size):
-                if is_clique(graph, subset):
-                    smaller_cliques.append(set(subset))
-    return sorted(map(sorted, smaller_cliques))  # Deduplicate and sort
+    for triple in combinations(nodes, 3):
+        if triple[0] in graph[triple[1]] and triple[0] in graph[triple[2]] and \
+           triple[1] in graph[triple[2]]:
+            cliques_of_size_3.add(tuple(sorted(triple)))
+
+    return cliques_of_size_3
 
 if __name__ == '__main__':
     with open('input') as fd:
@@ -35,14 +25,8 @@ if __name__ == '__main__':
             connections[a].add(b)
             connections[b].add(a)
 
-        print(connections)
-        cliques = []
-        bron_kerbosch(set(), set(connections.keys()), set(), connections, cliques)
-
-        smaller_cliques = find_all_subcliques(connections, cliques, 3)
-
-        cliques += smaller_cliques
+        cliques = find_cliques(connections)
 
         print([c for c in cliques if len(c) == 3])
-        print({tuple(c) for c in cliques if len(c) == 3 and any('t' in n for n in c)})
-        print(len({tuple(c) for c in cliques if len(c) == 3 and any('t' in n for n in c)}))
+        print({tuple(c) for c in cliques if len(c) == 3 and any(n.startswith('t') for n in c)})
+        print(len({tuple(c) for c in cliques if len(c) == 3 and any(n.startswith('t') for n in c)}))
